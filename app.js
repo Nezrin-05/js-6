6192744d6a11bffb8bee3a8213845fbd7cf57ca9
Nezrin-05 <nezrinnuriyeva70@gmail.com>
2024-08-31 15:35:48 +0400

    js 6

---
+++ app.js
@@ -0,0 +1,131 @@
+
+// videodaki tapsiriq
+
+// let id = prompt('Mehsul ID daxil edin.');
+// switch (id){
+//     case "1":
+//         console.log('Movcud 10 eded.');
+//         break;
+//         case "2":
+//             console.log('Movcud 256 eded.');
+//             break;
+//         case "3":
+//         console.log('53 eded movcuddur.');
+//         break;
+//         case "4":
+//         console.log('Movcud 3 eded.');
+//         break;
+//     default:
+//         console.log('Movcud deyil')
+//         break;
+// }
+
+// let gender = prompt("cinsinizi yazin");
+// gender == "M"|| gender == "m" ? console.log("Male") : console.log("Famele");
+
+
+
+// birinci tapsiriq
+
+// let month = prompt("Istediyiniz ayin nomresini yazin."); 
+// if (month === "1") {
+//     console.log("Yanvar")
+// }
+// else if(month == "2"){
+//     console.log("Fevral");
+    
+// }
+// else if(month == "3"){
+//     console.log("Mart");
+    
+// }
+// else if(month == "4"){
+//     console.log("Aprel");
+    
+// }
+// else if(month == "5"){
+//     console.log("May");
+    
+// }
+// else if(month == "6"){
+//     console.log("Iyun");
+    
+// }
+// else if(month == "7"){
+//     console.log("Iyul");
+    
+// }
+// else if(month == "8"){
+//     console.log("Avqust");
+    
+// }
+// else if(month == "9"){
+//     console.log("Sentyabr");
+    
+// }
+// else if(month == "10"){
+//     console.log("Oktyabr");
+    
+// }
+// else if(month == "11"){
+//     console.log("Noyabr");
+    
+// }
+// else if(month == "12"){
+//     console.log("Dekabr");
+    
+// }
+// else {
+// console.log("Netice tapilmadi. Xais olunur 1 ve 12 arasi reqem girin!");
+
+// }
+
+
+
+
+
+// ikinci tapsiriq
+
+// let date = prompt("Tarixi YYYY.MM.DD sekilinde daxil edin")
+// let year = date.substring(0,4);
+// let month = date.substring(5,7);
+// let day = date.substring(8,10);
+// let monthName;
+// if (month === "01"){
+//     monthName = "Yanvar"
+// }
+// else if (month=== "02"){
+//     monthName = "Fevral"
+// }
+// else if (month=== "03"){
+//     monthName = "Mart"
+// }
+// else if (month=== "04"){
+//     monthName = "Aprel"
+// }
+// else if (month=== "05"){
+//     monthName = "May"
+// }
+// else if (month=== "06"){
+//     monthName = "Iyun"
+// }
+// else if (month=== "07"){
+//     monthName = "Iyul"
+// }
+// else if (month=== "08"){
+//     monthName = "Avqust"
+// }
+// else if (month=== "09"){
+//     monthName = "Sentyabr"
+// }
+// else if (month=== "10"){
+//     monthName = "Oktyabr"
+// }
+// else if (month=== "11"){
+//     monthName = "Noyabr"
+// }
+// else if (month=== "12"){
+//     monthName = "Dekabr"
+// }
+// let resultData = day + " " +monthName + " " + year + "-cu il";
+// console.log(resultData);
